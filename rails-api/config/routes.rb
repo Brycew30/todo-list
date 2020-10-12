2a6855ba0b831fdb204ed10f25a1c5bc65e29fa0
Brycew30 <brycew30@gmail.com>
2020-10-12 11:31:28 -0500

    make list show route by name

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/lists' => 'lists#index'
-  get '/lists/:item_name' => 'lists#show'
+  get '/lists/:title' => 'lists#show'
   post '/lists' => 'lists#create'
   get '/items' => 'items#index'
   get '/items/:id' => 'items#show'
